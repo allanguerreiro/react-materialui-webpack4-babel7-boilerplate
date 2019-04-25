84c3123170271271c84bbc8d9ea6db100ca21141
allancarneiro <allan.carneiro@gft.com>
2019-04-24 21:43:09 -0300

    first commit

---
+++ src/containers/AppTheme/AppTheme.js
@@ -0,0 +1,20 @@
+import {MuiThemeProvider} from '@material-ui/core/es/styles'
+import React, {Component} from 'react'
+
+export default class AppTheme extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+
+        const {theme, children} = this.props;
+        return (
+            <MuiThemeProvider theme={theme}>
+                {children}
+            </MuiThemeProvider>
+        )
+    }
+}
